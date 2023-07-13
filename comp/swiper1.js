0e5a14966745b901b3602801017b88cd50265616
RaoufSiblani <siblani.raouf13@gmail.com>
2023-07-13 20:56:26 +0300

    asdfghj

--- comp/swiper1.js
+++ comp/swiper1.js
@@ -1,36 +1,49 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Wid2 from './widget2';
 
 const widgetData = [
-    { imageSrc: "/images/cata1.png", heading: "Design and Development" },
-    { imageSrc: "/images/cata2.png", heading: "Accounting and Finance" },
-    { imageSrc: "/images/cata3.png", heading: "Bank Institution" },
-    { imageSrc: "/images/cata4.png", heading: "Product Management" },
-    { imageSrc: "/images/cata5.png", heading: "Customer Support" },
-  ];
-  const Swiper1 = () => {
+  { imageSrc: "/images/cata1.png", heading: "Design and Development" },
+  { imageSrc: "/images/cata2.png", heading: "Accounting and Finance" },
+  { imageSrc: "/images/cata3.png", heading: "Bank Institution" },
+  { imageSrc: "/images/cata4.png", heading: "Product Management" },
+  { imageSrc: "/images/cata5.png", heading: "Customer Support" },
+];
+
+const Swiper1 = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      setIsLargeScreen(window.innerWidth >= 992);
+    };
+
+    checkWindowSize();
+
+    window.addEventListener('resize', checkWindowSize);
+    return () => {
+      window.removeEventListener('resize', checkWindowSize);
+    };
+  }, []);
+
+  const centeredSlides = isLargeScreen ? false : true;
+  const slidesPerView = isLargeScreen ? 4.2 : 1;
+
   return (
     <div>
-      <Swiper
-        slidesPerView={4.2}
-        spaceBetween={40}
-        centeredSlides={false}
-        className="hidden lg:block"
-      >
-         {widgetData.map((widget, index) => (
-            <SwiperSlide>
-          <Wid2
-            key={index}
-            imageSrc={widget.imageSrc}
-            heading={widget.heading}
-          />
+      <Swiper slidesPerView={slidesPerView} spaceBetween={40} centeredSlides={centeredSlides}>
+        {widgetData.map((widget, index) => (
+          <SwiperSlide key={index}>
+            <Wid2
+              imageSrc={widget.imageSrc}
+              heading={widget.heading}
+            />
           </SwiperSlide>
-         ))}
+        ))}
       </Swiper>
-      </div>
+    </div>
   );
 };
 
--- comp/swiper2.js
+++ comp/swiper2.js
@@ -1,33 +1,46 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Wid3 from './widget3';
 
 const widgetData = [
-    { imageSrc: "/images/job1.png", heading: "UI/UX Designer", company: "Dimension Studio" },
-    { imageSrc: "/images/job2.png", heading: "Full Stack Developer", company: "Alpander"  },
-    { imageSrc: "/images/job3.png", heading: "Product Designer", company: "XReact Tech" }
-  ];
+  { imageSrc: "/images/job1.png", heading: "UI/UX Designer", company: "Dimension Studio" },
+  { imageSrc: "/images/job2.png", heading: "Full Stack Developer", company: "Alpander"  },
+  { imageSrc: "/images/job3.png", heading: "Product Designer", company: "XReact Tech" }
+];
 
 const Swiper2 = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      setIsLargeScreen(window.innerWidth >= 992);
+    };
+
+    checkWindowSize();
+
+    window.addEventListener('resize', checkWindowSize);
+    return () => {
+      window.removeEventListener('resize', checkWindowSize);
+    };
+  }, []);
+
+  const centeredSlides = isLargeScreen ? false : true;
+  const slidesPerView = isLargeScreen ? 2.7 : 1;
+
   return (
-      <Swiper
-        slidesPerView={2.7}
-        spaceBetween={40}
-        centeredSlides={false}
-      >
-         {widgetData.map((widget, index) => (
-            <SwiperSlide>
+    <Swiper slidesPerView={slidesPerView} spaceBetween={40} centeredSlides={centeredSlides}>
+      {widgetData.map((widget, index) => (
+        <SwiperSlide key={index}>
           <Wid3
-            key={index}
             imageSrc={widget.imageSrc} 
             heading={widget.heading} 
             company={widget.company}
           />
-          </SwiperSlide>
-         ))}
-      </Swiper>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
