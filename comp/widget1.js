17314af744695798b4e39e67ca2b89703539ff57
RaoufSiblani <siblani.raouf13@gmail.com>
2023-07-19 12:00:41 +0300

    Update widget1.js

--- comp/widget1.js
+++ comp/widget1.js
@@ -1,8 +1,8 @@
 const Wid1 = ({ imageSrc, heading, paragraph }) => {
   return (
     <div className="bg-white rounded-xl pl-7 py-12">
-      <img src={imageSrc} className="h-14" />
-      <h2 className="font-bold text-2xl lg:text-3xl py-5">{heading}</h2>
+      <img src={imageSrc} className="lg:h-14 h-6" />
+      <h2 className="font-bold text-xl lg:text-3xl py-5">{heading}</h2>
       <p className="text-left text-sm lg:text-base text-textgray">{paragraph}</p>
     </div>
   );
