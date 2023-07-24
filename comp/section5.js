89e852ed8fc19704caccd6f577fb72842a5efbf1
RaoufSiblani <siblani.raouf13@gmail.com>
2023-07-25 02:50:28 +0300

    igga

--- comp/section5.js
+++ comp/section5.js
@@ -11,9 +11,9 @@ const Section5 = () => {
 
   return (
     <div className="items-center  lg:py-28 py-12">
-      <div className="pl-4  lg:pl-32">
-      <h2 className="font-bold text-4xl lg:text-5xl mt-10 leading-tight text-center pr-16 lg:pr-32">Recently Added Jobs</h2>
-        <p className="text-2xl lg:text-3xl text-textgray text-center mt-10 pr-16 lg:pr-32">
+      <div className="px-4  lg:pl-32">
+      <h2 className="font-bold text-4xl lg:text-5xl mt-10 leading-tight text-center lg:pr-32">Recently Added Jobs</h2>
+        <p className="text-lg lg:text-3xl text-textgray text-center mt-10 px-16 lg:px-64">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus non
           orci euismod vestibulum vitae ut ex. Quisque ut arcu at lectus tristique
           auctor sit amet at turpis.
--- comp/section6.js
+++ comp/section6.js
@@ -1,9 +1,29 @@
 const Section6 = () => {
     return (
-        <div className="px-32 lg:px-2"> 
+        <div className="lg:px-32 px-4"> 
         <h2 className="font-bold text-2xl mt-10 leading-tight text-center lg:text-left  lg:text-3xl">People's Feedbck about JobNow!</h2>
         <div className="grid grid-cols-12">
+            <div className="col-span-12 lg:col-span-7">
+            <p className="text-lg lg:text-2xl text-textgray text-center lg:text-left mt-10 ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus non
+          orci euismod vestibulum vitae ut ex.
+        </p>
+        <p className="text-xl lg:text-3xl  text-center lg:text-left mt-10 ">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus non
+          orci euismod vestibulum vitae ut ex. Quisque ut arcu at lectus tristique
+          auctor sit amet at turpis."
+        </p>
+        </div>
+            <div className="col-span-12 lg:col-span-5 mt-20">
+                <img src="/images/placeholder6.png" className=" w-5/6 mx-auto rounded-xl" />
+            </div>
+            <div className="col-span-12">
+            <h3 className="text-xl font-bold lg:text-3xl  text-center lg:text-left mt-10">Brown Garcia</h3>
+            <p className="text-lg lg:text-2xl text-textgray text-center lg:text-left mt-10 ">Full Stack Developer in XReact Tech</p>
+            <div className="flex mx-auto">
 
+            </div>
+            </div>
         </div>
         </div>
         );
