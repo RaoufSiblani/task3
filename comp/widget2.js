aec2307d164e5aa0dfc23c732b4313307ed57c6e
RaoufSiblani <siblani.raouf13@gmail.com>
2023-07-25 14:48:05 +0300

    asdfghj

--- comp/widget2.js
+++ comp/widget2.js
@@ -1,10 +1,14 @@
 const Wid2 = ({ imageSrc, heading }) => {
   return (
-    <div className="bg-white rounded-xl lg:w-80 w-60 h-60 lg:h-80 px-9 flex flex-col items-center justify-center">
-  <img src={imageSrc} className="lg:h-20 lg:w-20 h-10 w-10 flex mt-10 mx-auto" />
-  <h2 className="font-bold text-center lg:text-xl text-base flex py-8">{heading}</h2>
-  <p className="lg:text-left text-center lg:text-base text-sm text-textgray flex mb-10">350 Job Vacancy</p>
-</div>
+    <div className="bg-white rounded-xl lg:w-80 w-60 h-60 lg:h-80 px-9 flex flex-col items-center justify-center group hover:bg-maingreen hover:text-white">
+      <img
+        src={imageSrc}
+        className="lg:h-20 lg:w-20 h-10 w-10 flex mt-10 mx-auto filter group-hover:brightness-0 group-hover:invert"
+        alt="Image"
+      />
+      <h2 className="font-bold text-center lg:text-xl text-base flex py-8">{heading}</h2>
+      <p className="lg:text-left text-center lg:text-base text-sm text-textgray flex mb-10">350 Job Vacancy</p>
+    </div>
   );
 };
 
